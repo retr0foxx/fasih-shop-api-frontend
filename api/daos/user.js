06d163f203c4916371222735c91b614d43246189
fasihh <fhk.tcs@gmail.com>
2024-06-16 16:13:51 +0500

    removed sync

--- api/daos/user.js
+++ api/daos/user.js
@@ -1,20 +1,11 @@
 const User = require('../models/user');
-const sync = require('../utils/sync');
 
-module.exports.getAll = async () => {
-    await sync();
-    return await User.findAll(); 
-}
+module.exports.getAll = async () => await User.findAll();
 
-module.exports.getByName = async username => {
-    await sync();
-    return await User.findOne({ where: { username }});
-}
+module.exports.getByName = async username => await User.findOne({ where: { username }});
 
-module.exports.create = async user => {
-    await sync();
+module.exports.create = async user => 
     await User.create({
         username: user.username,
         password: user.password,
     });
-}
