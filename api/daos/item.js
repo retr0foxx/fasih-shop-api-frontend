ee601a3d56c102e0f1fdd4b131e633a632897df3
fasihh <fhk.tcs@gmail.com>
2024-06-16 16:15:24 +0500

    removed sync

--- api/daos/item.js
+++ api/daos/item.js
@@ -1,33 +1,20 @@
 const Item = require('../models/item');
-const sync = require('../utils/sync');
 
-module.exports.getAll = async options => {
-    await sync();
-    return await Item.findAll(options);
-}
+module.exports.getAll = async options => 
+    await Item.findAll(options);
 
-module.exports.getById = async id => {
-    await sync();
-    return await Item.findOne({ where: { id } });
-}
+module.exports.getById = async id =>
+    await Item.findOne({ where: { id } });
 
-module.exports.countAll = async options => {
-    await sync();
-    return await Item.count(options);
-}
+module.exports.countAll = async options =>
+    await Item.count(options);
 
-module.exports.create = async item => {
-    await sync();
+module.exports.create = async item =>
     await Item.create(item);
-}
 
-module.exports.update = async item => {
-    await sync();
+module.exports.update = async item =>
     await Item.update(item, { where: { id: item.id } });
-}
 
 // only needs id but using this to keep it all uniform
-module.exports.delete = async item => {
-    await sync();
+module.exports.delete = async item =>
     await Item.destroy({ where: { id: item.id } });
-}
--- api/utils/sync.js
+++
@@ -1,4 +0,0 @@
-const sequelize = require('../../db');
-require('dotenv').config();
-
-module.exports = async () => await sequelize.sync({ alter: process.env.MODE === 'dev' });
