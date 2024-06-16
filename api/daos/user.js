32399b1a3a979b69602073aa226522230665468f
fasihh <fhk.tcs@gmail.com>
2024-06-16 16:17:40 +0500

    small fix

--- api/daos/user.js
+++ api/daos/user.js
@@ -7,7 +7,6 @@ module.exports.getByName = async username =>
     await User.findOne({ where: { username }});
 
 module.exports.create = async user => 
-module.exports.create = async user => 
     await User.create({
         username: user.username,
         password: user.password,
